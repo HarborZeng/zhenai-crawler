e39d69aef5f1ded71d371398acb5a76f0f898b9a
harbo <757772438@qq.com>
2018-11-10 12:32:37 +0800

    抽取worker

--- crawler/engine/engine.go
+++ crawler/engine/engine.go
@@ -15,19 +15,27 @@ func Run(seeds ...Request) {
 		r := requests[0]
 		requests = requests[1:]
 
-		log.Printf("Fetching URL: %s", r.Url)
-
-		body, err := fetcher.Fetch(r.Url)
+		parseResult, err := worker(r)
 		if err != nil {
-			log.Printf("Fetcher: error fetching url %s: %v", r.Url, err)
 			continue
 		}
 
-		parseResult := r.ParserFunc(body)
 		requests = append(requests, parseResult.Requests...)
 
 		for _, item := range parseResult.Items {
 			log.Printf("Fetched item %v", item)
 		}
 	}
+}
+
+func worker(r Request) (ParseResult, error) {
+	log.Printf("Fetching URL: %s", r.Url)
+	body, err := fetcher.Fetch(r.Url)
+	if err != nil {
+		log.Printf("Fetcher: error fetching url %s: %v", r.Url, err)
+		return ParseResult{}, err
+	}
+
+	return r.ParserFunc(body), nil
+
 }
