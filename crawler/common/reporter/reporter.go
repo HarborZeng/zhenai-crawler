2e0bada4e820037ce758faa7d44b9d3a4121a180
harbo <757772438@qq.com>
2018-11-12 11:08:33 +0800

    fix ftqq report without description

--- crawler/common/reporter/reporter.go
+++ crawler/common/reporter/reporter.go
@@ -7,7 +7,7 @@ import (
 const ftUrl = "https://sc.ftqq.com/SCU6693T4108bacae69e4d52fbb8dd73f048bb185b6d687b94350.send"
 
 func ReportError(title string, err error) {
-	http.Get(ftUrl + "?text=" + title + "&desc=" + err.Error())
+	http.Get(ftUrl + "?text=" + title + "&desp=" + err.Error())
 }
 
 func ReportMessage(title string) {
